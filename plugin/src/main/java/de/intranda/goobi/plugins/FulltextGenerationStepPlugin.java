2356917853800c56d37fd9b67f43310e05359994
Robert Sehr <robert.sehr@intranda.com>
2022-07-29 15:46:59 +0200

    get thumbnails and fulltext from pdf files

--- plugin/src/main/java/de/intranda/goobi/plugins/FulltextGenerationStepPlugin.java
+++ plugin/src/main/java/de/intranda/goobi/plugins/FulltextGenerationStepPlugin.java
@@ -1,5 +1,10 @@
 package de.intranda.goobi.plugins;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * This file is part of a plugin for Goobi - a Workflow tool for the support of mass digitization.
  *
@@ -20,8 +25,11 @@ package de.intranda.goobi.plugins;
  */
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.commons.configuration.SubnodeConfiguration;
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.goobi.beans.Process;
 import org.goobi.beans.Step;
 import org.goobi.production.enums.PluginGuiType;
 import org.goobi.production.enums.PluginReturnValue;
@@ -29,7 +37,14 @@ import org.goobi.production.enums.PluginType;
 import org.goobi.production.enums.StepReturnValue;
 import org.goobi.production.plugin.interfaces.IStepPluginVersion2;
 
+import de.intranda.digiverso.pdf.PDFConverter;
+import de.intranda.digiverso.pdf.exception.PDFReadException;
+import de.intranda.digiverso.pdf.exception.PDFWriteException;
 import de.sub.goobi.config.ConfigPlugins;
+import de.sub.goobi.config.ConfigurationHelper;
+import de.sub.goobi.helper.StorageProvider;
+import de.sub.goobi.helper.exceptions.DAOException;
+import de.sub.goobi.helper.exceptions.SwapException;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
 import net.xeoh.plugins.base.annotations.PluginImplementation;
@@ -37,14 +52,19 @@ import net.xeoh.plugins.base.annotations.PluginImplementation;
 @PluginImplementation
 @Log4j2
 public class FulltextGenerationStepPlugin implements IStepPluginVersion2 {
-    
+
+    // for epub: install calibre or epub2txt2
+    // for pdf: install ghostscript
+
+    private static final String DEFAULT_ENCODING = "utf-8";
+
     @Getter
     private String title = "intranda_step_fulltext_generation";
     @Getter
     private Step step;
     @Getter
     private String value;
-    @Getter 
+    @Getter
     private boolean allowTaskFinishButtons;
     private String returnPath;
 
@@ -52,20 +72,17 @@ public class FulltextGenerationStepPlugin implements IStepPluginVersion2 {
     public void initialize(Step step, String returnPath) {
         this.returnPath = returnPath;
         this.step = step;
-                
+
         // read parameters from correct block in configuration file
         SubnodeConfiguration myconfig = ConfigPlugins.getProjectAndStepConfig(title, step);
-        value = myconfig.getString("value", "default value"); 
+        value = myconfig.getString("value", "default value");
         allowTaskFinishButtons = myconfig.getBoolean("allowTaskFinishButtons", false);
         log.info("FulltextGeneration step plugin initialized");
     }
 
     @Override
     public PluginGuiType getPluginGuiType() {
-        return PluginGuiType.FULL;
-        // return PluginGuiType.PART;
-        // return PluginGuiType.PART_AND_FULL;
-        // return PluginGuiType.NONE;
+        return PluginGuiType.NONE;
     }
 
     @Override
@@ -87,7 +104,7 @@ public class FulltextGenerationStepPlugin implements IStepPluginVersion2 {
     public String finish() {
         return "/uii" + returnPath;
     }
-    
+
     @Override
     public int getInterfaceVersion() {
         return 0;
@@ -97,7 +114,7 @@ public class FulltextGenerationStepPlugin implements IStepPluginVersion2 {
     public HashMap<String, StepReturnValue> validate() {
         return null;
     }
-    
+
     @Override
     public boolean execute() {
         PluginReturnValue ret = run();
@@ -106,13 +123,78 @@ public class FulltextGenerationStepPlugin implements IStepPluginVersion2 {
 
     @Override
     public PluginReturnValue run() {
-        boolean successful = true;
-        // your logic goes here
-        
-        log.info("FulltextGeneration step plugin executed");
-        if (!successful) {
+
+        Process process = step.getProzess();
+        MutableInt counter = new MutableInt(1);
+        // list files
+        List<Path> originalFiles = null;
+        try {
+            String imageFolder = process.getImagesTifDirectory(false);
+            originalFiles = StorageProvider.getInstance().listFiles(imageFolder);
+        } catch (IOException | InterruptedException | SwapException | DAOException e) {
+            log.error(e);
             return PluginReturnValue.ERROR;
         }
+        if (originalFiles != null && !originalFiles.isEmpty()) {
+            Path textFolder = null;
+            Path pdfFolder = null;
+            Path altoFolder = null;
+            Path imagesFolder = null;
+            try {
+                textFolder = Paths.get(process.getOcrTxtDirectory());
+                pdfFolder = Paths.get(process.getOcrPdfDirectory());
+                altoFolder = Paths.get(process.getOcrAltoDirectory());
+                imagesFolder = Paths.get(process.getThumbsDirectory(), process.getTitel() + "_media");
+                if (!StorageProvider.getInstance().isFileExists(textFolder)) {
+                    StorageProvider.getInstance().createDirectories(textFolder);
+                }
+                if (!StorageProvider.getInstance().isFileExists(pdfFolder)) {
+                    StorageProvider.getInstance().createDirectories(pdfFolder);
+                }
+                if (!StorageProvider.getInstance().isFileExists(altoFolder)) {
+                    StorageProvider.getInstance().createDirectories(altoFolder);
+                }
+                if (!StorageProvider.getInstance().isFileExists(imagesFolder)) {
+                    StorageProvider.getInstance().createDirectories(imagesFolder);
+                }
+
+            } catch (SwapException | DAOException | IOException | InterruptedException e) {
+                log.error(e);
+                return PluginReturnValue.ERROR;
+            }
+
+            for (Path source : originalFiles) {
+                // check file extension
+                // if pdf
+                if (source.getFileName().toString().toLowerCase().endsWith(".pdf")) {
+                    try {
+                        PDFConverter.writeFullText(source.toFile(), textFolder.toFile(), DEFAULT_ENCODING, counter.toInteger());
+                        List<File> imageFiles = PDFConverter.writeImages(source.toFile(), imagesFolder.toFile(), counter.toInteger(), 300, "tif",
+                                new File(ConfigurationHelper.getInstance().getTemporaryFolder()), "ghostscript");
+                        List<File> singlePagePdfs = PDFConverter.writeSinglePagePdfs(source.toFile(), pdfFolder.toFile(), 1);
+                        for (int i = 0; i < singlePagePdfs.size(); i++) {
+                            File pdfFile = singlePagePdfs.get(i);
+                            File imageFile = null;
+                            if (i < imageFiles.size()) {
+                                imageFile = imageFiles.get(i);
+                            }
+                            PDFConverter.writeAltoFile(pdfFile, altoFolder.toFile(), imageFile, false);
+                        }
+                        counter.add(Math.max(singlePagePdfs.size(), imageFiles.size()));
+                    } catch (PDFReadException | PDFWriteException e) {
+                        log.error(e);
+                        return PluginReturnValue.ERROR;
+                    }
+                }
+                // if epub
+                else if (source.getFileName().toString().toLowerCase().endsWith(".epub")) {
+                    // call cli command from config
+                }
+
+            }
+
+        }
+
         return PluginReturnValue.FINISH;
     }
 }
